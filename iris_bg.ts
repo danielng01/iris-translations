7f66fc1f75979f06cc3c0d755db9147a0750d1cd
Daniel Georgiev <daniel.georgiev95@gmail.com>
2019-05-15 13:15:45 +0300

    change translations

--- iris_bg.ts
+++ iris_bg.ts
@@ -1499,7 +1499,7 @@ Smart inversion on OSX is CPU intensive and may slow down your computer a lot. U
     <message>
         <location filename="../../src/translation/Translator.cpp" line="433"/>
         <source>ADVANCED_SETTINGS_TAB</source>
-        <translation>Още Настройки</translation>
+        <translation>Още настройки</translation>
     </message>
     <message>
         <location filename="../../src/translation/Translator.cpp" line="434"/>
